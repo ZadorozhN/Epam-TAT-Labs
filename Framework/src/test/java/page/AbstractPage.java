d8b8c9bdbe6f1997bbf8ced7b419cd33c074ecfe
zadorozhn <zadorozhny.nik@gmail.com>
2020-12-09 18:19:20 +0300

    some clean code changes of abstract classes

--- Framework/src/test/java/page/AbstractPage.java
+++ Framework/src/test/java/page/AbstractPage.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-
 public abstract class AbstractPage {
     protected final int WAIT_TIMEOUT_SECONDS = 300;
 
@@ -20,63 +18,53 @@ public abstract class AbstractPage {
 
     public WebElement waitUntilPresenceOfElement(By location){
         return new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
-                .pollingEvery(Duration.ofSeconds(2))
                 .until(ExpectedConditions.presenceOfElementLocated(location));
     }
 
     public WebElement waitUntilVisibilityOf(WebElement element){
         return new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
-                .pollingEvery(Duration.ofSeconds(2))
                 .until(ExpectedConditions.visibilityOf(element));
     }
 
     public WebElement waitUntilElementIsClickable(By location){
         return new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
-                .pollingEvery(Duration.ofSeconds(2))
                 .until(ExpectedConditions.elementToBeClickable(location));
     }
 
     public void waitUntilElementIsClickableAndClickAvoidModalWindow(By location){
         try {
             new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
-                    .pollingEvery(Duration.ofSeconds(2))
                     .until(ExpectedConditions.elementToBeClickable(location)).click();
         } catch (ElementClickInterceptedException exception){
             driver.navigate().refresh();
             new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
-                    .pollingEvery(Duration.ofSeconds(2))
                     .until(ExpectedConditions.elementToBeClickable(location)).click();
         }
     }
 
     public WebElement waitUntilElementIsClickable(WebElement element){
         return new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
-                .pollingEvery(Duration.ofSeconds(2))
                 .until(ExpectedConditions.elementToBeClickable(element));
     }
 
     public void waitUntilElementIsClickableAndClickAvoidModalWindow(WebElement element){
         try {
             new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
-                    .pollingEvery(Duration.ofSeconds(2))
                     .until(ExpectedConditions.elementToBeClickable(element)).click();
         } catch (ElementClickInterceptedException exception){
             driver.navigate().refresh();
             new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
-                    .pollingEvery(Duration.ofSeconds(2))
                     .until(ExpectedConditions.elementToBeClickable(element)).click();
         }
     }
 
     public void waitUntilFieldIsChanged(WebElement element, String startValue){
         new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
-                .pollingEvery(Duration.ofSeconds(2))
                 .until(ExpectedConditions.not(ExpectedConditions.textToBePresentInElement(element, startValue)));
     }
 
     public void waitUntilAjaxCompleted(){
         new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
-                .pollingEvery(Duration.ofSeconds(2))
                 .until(jQueryAJAXCompleted());
     }
 
